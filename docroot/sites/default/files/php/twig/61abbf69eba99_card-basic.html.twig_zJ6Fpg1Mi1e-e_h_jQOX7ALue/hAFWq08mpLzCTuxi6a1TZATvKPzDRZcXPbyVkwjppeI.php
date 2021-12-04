379371b8df25aca977a70925b2024d4fa9d9adf9
edsonmarinh0 <marinhoedson1@gmail.com>
2021-12-04 18:37:55 -0300

    build: implemente módulo path auto
    implementação do módulo path auto

---
+++ docroot/sites/default/files/php/twig/61abbf69eba99_card-basic.html.twig_zJ6Fpg1Mi1e-e_h_jQOX7ALue/hAFWq08mpLzCTuxi6a1TZATvKPzDRZcXPbyVkwjppeI.php
@@ -0,0 +1,99 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/contrib/bootstrap_barrio/layouts/card-basic.html.twig */
+class __TwigTemplate_c0cebace70173231374416ffa19aee726cc402f4ee0531278c0fab73b8a52c71 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 13
+        echo "<div";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => "card", 1 => "clearfix", 2 => "card-block", 3 => "card-body"], "method", false, false, true, 13), 13, $this->source), "html", null, true);
+        echo ">
+
+  ";
+        // line 15
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "first", [], "any", false, false, true, 15), 15, $this->source), "html", null, true);
+        echo "
+
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/contrib/bootstrap_barrio/layouts/card-basic.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  45 => 15,  39 => 13,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/contrib/bootstrap_barrio/layouts/card-basic.html.twig", "/var/www/html/docroot/themes/contrib/bootstrap_barrio/layouts/card-basic.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 13);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
